5c7838f12af7457e9f7727886a00f5455d8cdf58
chanwon moon <kogoome@kakao.com>
2022-04-06 22:12:57 +0900

    정리 푸시

--- src/routes/roadmap/index.svelte
+++ src/routes/roadmap/index.svelte
@@ -1,6 +1,6 @@
 <div class="bg-blue-200 h-screen">
-  <div class="flex justify-start h-full">
-    <div class="relative top-20 w-1/3 left-44 flex flex-col text-2xl">
+  <div class="flex justify-start h-full p-20">
+    <div class="relative w-1/3 flex flex-col text-2xl">
       <h1>목표</h1>
       <div>웹 앱 제작</div>
       <div>Eletron 데스크탑 앱 제작</div>
@@ -14,14 +14,14 @@
       <div>웹 구조 및 아이콘 갖추기</div>
       <div>타이머 컴포넌트 작성</div>
       <h1>22.4.6</h1>
-      <div>네비메뉴 재정리</div>
+      <div>네비메뉴 재정리-로그인 프로필 횐갑</div>
       <div>각 페이지 별 목표점 작성</div>
       <div>개인작업공간 공간구조 작성</div>
       <div>로드맵 페이지 작성</div>
       <div>로그인 모달 작성</div>
 
     </div>
-    <div class="relative top-20 w-1/3 flex flex-col text-2xl">
+    <div class="relative w-1/3 flex flex-col text-2xl">
       <h1>할일들</h1>
       <div>jwt Oauth 회원가입 기능</div>
       <div>로그인 기능과 세션</div>
@@ -29,8 +29,9 @@
       <div>메모 작성기능 구현</div>
       <div>검색기능 구현</div>
       <div>제텔카스텔 정리, 서지시스템 구현</div>
+      <div>타입스크립트 클래스 전환</div>
     </div>
-    <div class="relative top-20 w-1/3 flex flex-col text-2xl">
+    <div class="relative w-1/3 flex flex-col text-2xl">
       <h1>배울것</h1>
       <div>Canvas</div>
       <div>무한스크롤</div> 
@@ -38,14 +39,14 @@
       <div>Flutter</div>
 
     </div>
-  </div>
-</div>
-<div class="bg-blue-200 h-screen">
-  <div class="flex justify-start h-full">
-    <div class="relative top-20 w-1/3 left-44 flex flex-col text-2xl">
+    <div class="relative w-1/3 flex flex-col text-2xl">
+      <h1>더 적을거?</h1>
+
     </div>
   </div>
 </div>
+<div class="bg-blue-200 h-screen"></div>
+<div class="bg-blue-200 h-screen"></div>
 <style>
   hr { border: 1px solid #000; }
   h1 {
